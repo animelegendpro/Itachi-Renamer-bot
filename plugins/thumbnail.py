1db3a18d99b44cc27599f2869d7455b9e964d683
AutoAniZone <autoanizone@gmail.com>
2024-05-15 00:40:41 +0530

    Update thumbnail.py

--- plugins/thumbnail.py
+++ plugins/thumbnail.py
@@ -8,18 +8,18 @@ async def viewthumb(client, message):
     if thumb:
        await client.send_photo(chat_id=message.chat.id, photo=thumb)
     else:
-        await message.reply_text("**You Don't Have Any Thumbnail ❌**") 
+        await message.reply_text("**ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴀɴʏ ᴛʜᴜᴍʙɴᴀɪʟ ✘**") 
 		
 @Client.on_message(filters.private & filters.command(['del_thumb', 'delthumb']))
 async def removethumb(client, message):
     await jishubotz.set_thumbnail(message.from_user.id, file_id=None)
-    await message.reply_text("**Thumbnail Deleted Successfully 🗑️**")
+    await message.reply_text("**ᴛʜᴜᴍʙɴᴀɪʟ ᴅᴇʟᴇᴛᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ 🗑️**")
 	
 @Client.on_message(filters.private & filters.photo)
 async def addthumbs(client, message):
     mkn = await message.reply_text("Please Wait ...")
     await jishubotz.set_thumbnail(message.from_user.id, file_id=message.photo.file_id)                
-    await mkn.edit("**Thumbnail Saved Successfully ✅️**")
+    await mkn.edit("**ᴛʜᴜᴍʙɴᴀɪʟ sᴀᴠᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ ✓**")
 
 
 
